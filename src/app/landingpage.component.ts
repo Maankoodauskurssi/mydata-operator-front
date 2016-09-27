a942dcb24fcd41e47c34f079c355de849bd49620
anttiarp <antti.arponen@basware.com>
2016-09-27 13:35:52 +0300

    eka

---
+++ src/app/landingpage.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'landing-page',
+    templateUrl: './landingpage.component.html'
+})
+export class LandingPage {
+
+}
---
+++ src/main.browser.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { UniversalModule } from 'angular2-universal';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { App } from './app/app';
+import { NavbarComponent } from './app/navbar.component'
+import { LandingPage } from './app/landingpage.component'
+
+@NgModule({
+    bootstrap: [App],
+    declarations: [App, NavbarComponent, LandingPage],
+    imports: [
+        UniversalModule, // BrowserModule, HttpModule, and JsonpModule are included
+        FormsModule,
+        NgbModule,
+        RouterModule.forRoot([
+            { path: '', component: LandingPage, pathMatch: 'full' }
+        ])
+    ]
+})
+export class MainModule {
+
+}
