40d9b7bb75937d33e457d1fd6d25c407b0ed6e1e
anttiarp <antti.arponen@outlook.com>
2016-10-01 22:09:58 +0300

    lisätty service client ja dummy kutsu

---
+++ SRC/app/mydata-operator-service-client/model/models.ts
@@ -0,0 +1,6 @@
+export * from './DataController';
+export * from './Header';
+export * from './Request';
+export * from './Services';
+export * from './ServicesInner';
+export * from './ServicesInnerPurposes';
--- src/app/services/servicedescription.component.ts
+++ src/app/services/servicedescription.component.ts
@@ -1,8 +1,65 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import * as api from '../mydata-operator-service-client';
 
 
 @Component({
-  selector: 'navbar',
-  templateUrl: './servicedescription.component.html'
+    selector: 'navbar',
+    templateUrl: './servicedescription.component.html',
+    providers: [api.DefaultApi]
 })
-export class ServiceDescriptionPage {}
+export class ServiceDescriptionPage {
+    response: any;
+    errorMessage: String;
+
+    constructor(private _api: api.DefaultApi) {
+    }
+
+    submitConsent() {
+        var header: api.Header = {
+            version: '1',
+            jurisdiction: '',
+            iat: 1,
+            moc: '',
+            jti: '',
+            publicKey: '',
+            policyUrl: '',
+            sub: '',
+            sensitive: true,
+            spiCat: []
+        },
+            dataController: api.DataController = {
+                onBehalf: false,
+                contact: '',
+                org: '',
+                address: '',
+                email: '',
+                phone: ''
+            },
+            services: api.Services = [{
+                serviceName: '',
+                purposes: [{
+                    purpose: 'I dont know :(',
+                    consentType: '',
+                    purposeCategory: [''],
+                    piiCategory: [''],
+                    nonCorePurpose: true,
+                    purposeTermination: '',
+                    thirdPartyDisclosure: true,
+                    thirdPartyName: ''
+                }]
+            }],
+            req: api.Request = {
+                dataController: dataController,
+                header: header,
+                services: services
+            };
+
+        var requests = new Array<api.Request>(req);
+
+        this._api.consentRequestPost(requests).subscribe(
+            response => this.response = response,
+            error => this.errorMessage = <any>error
+        )
+    }
+}
+
