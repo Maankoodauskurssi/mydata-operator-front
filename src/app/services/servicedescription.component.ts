35abccae5984196080ddbd8ea5669f6d761893ca
linnamaa <anon@donotreply.fi>
2016-10-05 12:28:55 +0300

    filled with default example values

--- src/app/services/servicedescription.component.ts
+++ src/app/services/servicedescription.component.ts
@@ -15,37 +15,41 @@ export class ServiceDescriptionPage {
     }
 
     submitConsent() {
+        // consent specification header
         var header: api.Header = {
                 version: '1',
-                jurisdiction: '',
+                jurisdiction: 'fi',
                 iat: 1,
-                moc: '',
-                jti: '',
-                publicKey: '',
-                policyUrl: '',
-                sub: '',
+                moc: 'automatic.app.request',
+                jti: '7c80b99e-dff9-4931-b927-bbddc0eec307', // unique receipt id, generate?
+                // publicKey: '',
+                // policyUrl: '', --> moved to service privacy policy
+                // principal id
+                sub: 'eppu.esimerkki@eemail.fi',
                 sensitive: true,
-                spiCat: []
+                // Sensitive PI data category
+                spiCat: ['Financial/PurchaseHistory']
             },
             dataController: api.DataController = {
                 onBehalf: false,
-                contact: '',
-                org: '',
-                address: '',
-                email: '',
-                phone: ''
+                contact: 'support@sti.fi',
+                org: 'STi',
+                address: 'Testitie 1, Postimaa',
+                email: 'sti@sti.com',
+                phone: '12345667890'
             },
             services: api.Services = [{
-                serviceName: '',
+                serviceName: 'wwf.ecologinen.jalanjalki',
                 purposes: [{
-                    purpose: 'I dont know :(',
-                    consentType: '',
-                    purposeCategory: [''],
-                    piiCategory: [''],
-                    nonCorePurpose: true,
-                    purposeTermination: '',
-                    thirdPartyDisclosure: true,
-                    thirdPartyName: ''
+                    purpose: 'To calculate your traffic behaviour impact to the environment, we need to know your traffic related costs',
+                    consentType: 'explicit',
+                    policyUrl: 'http://www.wwf.org/ecoapp/privacypolicy.html',
+                    purposeCategory: ['1.core.function'],
+                    piiCategory: ['Financial/PurchaseHistory'],
+                    nonCorePurpose: false,
+                    purposeTermination: 'no-expiry',
+                    thirdPartyDisclosure: false
+                    //thirdPartyName: ''
                 }]
             }],
             req: api.Request = {
