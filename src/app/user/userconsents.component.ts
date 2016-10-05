7739a1cb3bc09528076d57a47e0b45cdefab15b4
anttiarp <antti.arponen@outlook.com>
2016-10-05 12:26:16 +0300

    consents UI generation from mock data

---
+++ src/app/user/userconsents.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'user-consents',
+    templateUrl: './userconsents.component.html'
+})
+export class UserConsentsPage {
+
+}
